13b917061df12c21bf9bcf120f25c7201ad24e60
VIGNESHGADDAM <93676679+Vigneshgaddam@users.noreply.github.com>
2025-10-04 12:52:05 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ from io import BytesIO
 import os
 
 # Download the user's logo image
-logo_path = 'WhatsApp-Image-2025-10-04-at-12.41.18-PM.jpg'
+logo_path = 'logo_yv_crafts.png'
 logo_img = Image.open(logo_path)
 
 # Resize logo for web app header usage
